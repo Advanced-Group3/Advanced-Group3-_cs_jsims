487acf4f6cadd9fe2af58acce817ad9df683cee2
hailuhak <hailuhak344@gmail.com>
2025-04-15 06:15:20 -0700

    ADD DB Connection

--- csjsimscore/src/persistence/DbConnection.java
+++ csjsimscore/src/persistence/DbConnection.java
@@ -4,17 +4,14 @@ import java.sql.Connection;
 import java.sql.SQLException;
 import oracle.jdbc.pool.OracleDataSource;
 
-/**
- *
- * @author ChalewT
- */
 public class DbConnection {
     public static Connection getConnection() throws SQLException {
         OracleDataSource ods = new OracleDataSource();
-        ods.setURL("jdbc:oracle:thin:@localhost:1521/FreePDB1");
-        ods.setUser("jsims");
-        ods.setPassword("0raPassword");
+        ods.setURL("jdbc:oracle:thin:@localhost:1521:XE");
+        ods.setUser("DATABASE");
+        ods.setPassword("1234");
         Connection conn = ods.getConnection();
+        System.out.println("success");
         return conn;
     }
 }
